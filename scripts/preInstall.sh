2400902b041aa884b11cd284cc228f0cf809076d
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-04-18 16:45:32 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,11 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+SECRET_PASSWORD=${SECRET_PASSWORD:-`openssl rand -hex 32`}
+DATABASE_URL=postgres://postgres:${ADMIN_PASSWORD}@rallly_db:5432/db
+
+cat << EOT >> ./.env
+
+SECRET_PASSWORD=$SECRET_PASSWORD
+DATABASE_URL=$DATABASE_URL
+EOT
